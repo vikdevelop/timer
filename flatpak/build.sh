999931dec102214c3ed3364f611d331645c46f42
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2021-10-28 22:56:50 +0200

    Update build.sh

--- flatpak/build.sh
+++ flatpak/build.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
-install -Dm755 -t /app/bin flatpak/run.sh
-install -Dm755 -t /app/src src/timer.py
-install -Dm755 -t /app/share/applications flatpak/com.github.vikdevelop.timer.desktop
-install -Dm755 -t /app/share/icons/hicolor/128x128/apps flatpak/icons/com.github.vikdevelop.timer.png
-install -Dm644 -t /app/share/metainfo flatpak/com.github.vikdevelop.timer.metainfo.xml
+install -D -t /app/bin timer.sh
+install -D -t /app src/timer.py
+install -D -t /app/share/applications flatpak/com.github.vikdevelop.timer.desktop
+install -D -t /app/share/icons/hicolor/128x128/apps flatpak/icons/com.github.vikdevelop.timer.png
+install -D -t /app/share/metainfo flatpak/com.github.vikdevelop.timer.metainfo.xml
