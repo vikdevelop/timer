cd018d37dca9cfd0af99d99d584b7921eaa20eec
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2021-11-02 18:18:56 +0100

    Delete build.sh

--- flatpak/build.sh
+++
@@ -1,7 +0,0 @@
-#/bin/sh
-
-install -D -t /app/bin timer.sh
-install -D -t /app src/timer.py
-install -D -t /app/share/applications flatpak/com.github.vikdevelop.timer.desktop
-install -D -t /app/share/icons/hicolor/128x128/apps flatpak/icons/com.github.vikdevelop.timer.png
-install -D -t /app/share/metainfo flatpak/com.github.vikdevelop.timer.metainfo.xml
