94f55062a7e93468a27595eae098b0c992d93592
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-06-29 19:56:01 +0200

    Create en.py

---
+++ translations/en.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# english translations of package Timer
+
+timer_title = "Timer"
+timer_running = "Timer is running ..."
+run_timer = "Start timer"
+stop_timer = "Stop timer"
+timing_finished = "Timing has been finished!"
+time_text = "Time: "
+timing_ended = "Timing was stopped"
+timer_quit = "Timer was ended"
