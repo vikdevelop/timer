8fd024b9cb1ad7906aae0a47ac7a527c1c494d48
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-06-29 19:55:14 +0200

    Create cs.py

---
+++ translations/cs.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Czech translations of package Timer
+
+timer_title = "Časovač"
+timer_running = "Časovač běží ..."
+run_timer = "Zahájit časování"
+stop_timer = "Zastavit časování"
+timing_finished = "Časování bylo dokončeno!"
+time_text = "Čas: "
+timing_ended = "Časování bylo zastaveno"
+timer_quit = "Časovač byl ukončen"
