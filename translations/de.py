6d526ebb6f421de858aea8a96a7a973a3f9d1555
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-07-02 17:41:24 +0200

    Create de.py

---
+++ translations/de.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# deutsch translations of package Timer
+
+timer_title = "Timer"
+timer_running = "Der Timer läuft ..."
+run_timer = "Startzeitpunktr"
+stop_timer = "Stopzeitpunktr"
+timing_finished = "Die Zeitmessung ist abgeschlossen!"
+time_text = "Zeit: "
+timing_ended = "Die Zeitmessung wurde gestoppt"
+timer_quit = "Timer wurde beendet"
