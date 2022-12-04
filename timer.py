39a0f54b572df92c38ed72e2c474948aa7f0e5a2
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-12-04 19:15:42 +0100

    Update timer.py

--- timer.py
+++ timer.py
@@ -34,31 +34,6 @@ elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=nb_NO.UTF-8':
 else:
     with open('/app/translations/en.json') as t:
         jT = json.load(t)
-            
-# Properties for Shortcuts Window
-property_01 = '                <property name="title" translatable="true" context="shortcut window">Show Keyboard shortcuts</property>'
-property_02 = '                <property name="title" translatable="true" context="shortcut window">Start timing</property>'
-property_03 = '                <property name="title" translatable="true" context="shortcut window">Stop timing</property>'
-property_04 = '                <property name="title" translatable="true" context="shortcut window">Quit</property>'
-for line in fileinput.input('/app/src/ui/shortcuts.ui', inplace=1):
-    if property_01 in line:
-        line = line.replace(property_01,f'                <property name="title" translatable="true" context="shortcut window">{jT["keyboard_shortcuts"]}</property>')
-    sys.stdout.write(line)
-    
-for line in fileinput.input('/app/src/ui/shortcuts.ui', inplace=2):
-    if property_02 in line:
-        line = line.replace(property_02,f'                <property name="title" translatable="true" context="shortcut window">{jT["run_timer"]}</property>')
-    sys.stdout.write(line)
-    
-for line in fileinput.input('/app/src/ui/shortcuts.ui', inplace=3):
-    if property_03 in line:
-        line = line.replace(property_03,f'                <property name="title" translatable="true" context="shortcut window">{jT["stop_timer"]}</property>')
-    sys.stdout.write(line)
-
-for line in fileinput.input('/app/src/ui/shortcuts.ui', inplace=4):
-    if property_04 in line:
-        line = line.replace(property_04,f'                <property name="title" translatable="true" context="shortcut window">{jT["close"]}</property>')
-    sys.stdout.write(line)
 
 # import main file - source code
 import src.main
