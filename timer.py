da4d0f10e36ecd77524e33d684d4002964bde5a9
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-12-18 14:51:40 +0100

    Set load translation files from URL
    for continuous localization from Hosted Weblate

--- timer.py
+++ timer.py
@@ -1,41 +1,42 @@
 import subprocess
 import json
-# Czech
-if subprocess.getoutput("locale | grep 'LANG'") == 'LANG=cs_CZ.UTF-8':
-    with open('/app/translations/cs.json') as t:
-        jT = json.load(t)
-# Italian
-elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=it_IT.UTF-8':
-    with open('/app/translations/it.json') as t:
-        jT = json.load(t)
-# Deutsch
-elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=de_DE.UTF-8':
-    with open('/app/translations/de.json') as t:
-        jT = json.load(t)
-# Russian
-elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=ru_RU.UTF-8':
-    with open('/app/translations/ru.json') as t:
-        jT = json.load(t)
-# Finnish
-elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=fi_FI.UTF-8':
-    with open('/app/translations/fi.json') as t:
-        jT = json.load(t)
-# French
-elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=fr_FR.UTF-8':
-    with open('/app/translations/fr.json') as t:
-        jT = json.load(t)
-# Norwegian (Bokmål)
-elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=nb_NO.UTF-8':
-    with open('/app/translations/nb_NO.json') as t:
-        jT = json.load(t)
-# Ukrainian
-elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=uk_UA.UTF-8':
-    with open('/app/translations/uk.json') as t:
-        jT = json.load(t)
-# English
-else:
-    with open('/app/translations/en.json') as t:
-        jT = json.load(t)
+from urllib.request import urlopen
+BASE_URL = 'https://raw.githubusercontent.com/vikdevelop/timer/main/translations/json'
+try:
+    # Czech
+    if subprocess.getoutput("locale | grep 'LANG'") == 'LANG=cs_CZ.UTF-8':
+        lang = 'cs.json'
+    # Italian
+    elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=it_IT.UTF-8':
+        lang = 'it.json'
+    # Deutsch
+    elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=de_DE.UTF-8':
+        lang = 'de.json'
+    # Russian
+    elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=ru_RU.UTF-8':
+        lang = 'ru.json'
+    # Finnish
+    elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=fi_FI.UTF-8':
+        lang = 'fi.json'
+    # French
+    elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=fr_FR.UTF-8':
+        lang = 'fr.json'
+    # Norwegian (Bokmål)
+    elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=nb_NO.UTF-8':
+        lang = 'nb_NO.json'
+    # Ukrainian
+    elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=uk_UA.UTF-8':
+        lang = 'uk.json'
+    # English
+    else:
+        lang = 'en.json'
+    # Open Translation from URL
+    locale = urlopen('{}/{}'.format(BASE_URL, lang))
+except OSError:
+    locale = open(f'/app/translations/{lang}')
+
+# Load JSON local file
+jT = json.load(locale)
 
 # import main file - source code
 import src.main
