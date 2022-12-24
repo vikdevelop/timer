fe3f9f935268dbaf4ed8123023bb765c87739f1f
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-12-24 11:48:11 +0100

    Update alarm.sh

--- src/alarm.sh
+++ src/alarm.sh
@@ -1,1 +1,1 @@
-for i in {1..20}; do ffplay -nodisp -autoexit /app/share/beeps/Oxygen-Im-Phone-Ring.ogg > /dev/null 2>&1; done
+for i in {1..70}; do ffplay -nodisp -autoexit /app/share/beeps/Oxygen-Im-Phone-Ring.ogg > /dev/null 2>&1; done
