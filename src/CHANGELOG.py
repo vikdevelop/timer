5175bd8b914cfb9d6f6abc4b683b9aab413d09bb
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-12-27 19:32:34 +0100

    Create CHANGELOG.py

---
+++ src/CHANGELOG.py
@@ -0,0 +1,6 @@
+# CHANGELOG.py
+release_27 = '\
+<ul>\
+<li>added option to action after finished timer "Play alarm clock". On timeout will be played ringstone and show message dialog</li>\
+<li>added option for remove all settings of Timer</li>\
+<li>aded new keyboard shortcuts: F1 for show about dialog, F2 for switch to dark theme, F3 for switch to system theme and F5 for remove all Timer settings</li></ul>'
