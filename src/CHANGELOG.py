824509c898f0c27ab56826119fbe93834ffa0dde
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-12-28 12:09:50 +0100

    Update CHANGELOG.py

--- src/CHANGELOG.py
+++ src/CHANGELOG.py
@@ -3,7 +3,7 @@ release_27 = '\
 <ul>\
 <li>added option to action after finished timer "Play alarm clock". On timeout will be played ringstone and show message dialog</li>\
 <li>added Spanish translations, thanks to @haggen88</li>\
-<li>added option for remove all settings of Timer to headerbar under buttton Reset time counter values</li>\
+<li>added option for remove all settings of Timer to headerbar next to Reset button</li>\
 <li>aded new keyboard shortcuts: F1 for show about dialog, F2 for switch to dark theme, F3 for switch to system theme and F5 for remove all Timer settings</li>\
 <li>minor UI changes</li>\
 </ul>'
