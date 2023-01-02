5ac8bf441ddbd637dff8074121cbb83cadaab5dc
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2023-01-02 14:59:20 +0100

    Update timer.py

--- timer.py
+++ timer.py
@@ -1,42 +1,44 @@
 import subprocess
 import json
+import os
 from urllib.request import urlopen
+from pathlib import Path
 BASE_URL = 'https://raw.githubusercontent.com/vikdevelop/timer/main/translations/json'
-try:
-    # Czech
-    if subprocess.getoutput("locale | grep 'LANG'") == 'LANG=cs_CZ.UTF-8':
-        lang = 'cs.json'
-    # Italian
-    elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=it_IT.UTF-8':
-        lang = 'it.json'
-    # Deutsch
-    elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=de_DE.UTF-8':
-        lang = 'de.json'
-    # Russian
-    elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=ru_RU.UTF-8':
-        lang = 'ru.json'
-    # Finnish
-    elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=fi_FI.UTF-8':
-        lang = 'fi.json'
-    # French
-    elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=fr_FR.UTF-8':
-        lang = 'fr.json'
-    # Norwegian (Bokmål)
-    elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=nb_NO.UTF-8':
-        lang = 'nb_NO.json'
-    # Ukrainian
-    elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=uk_UA.UTF-8':
-        lang = 'uk.json'
-    # Spanish
-    elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=es_ES.UTF-8':
-        lang = 'es.json'
-    # English
-    else:
-        lang = 'en.json'
-    # Open Translation from URL
-    locale = urlopen('{}/{}'.format(BASE_URL, lang))
-except OSError:
-    locale = open(f'/app/translations/{lang}')
+# Czech
+if subprocess.getoutput("locale | grep 'LANG'") == 'LANG=cs_CZ.UTF-8':
+    lang = 'cs.json'
+# Italian
+elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=it_IT.UTF-8':
+    lang = 'it.json'
+# Deutsch
+elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=de_DE.UTF-8':
+    lang = 'de.json'
+# Russian
+elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=ru_RU.UTF-8':
+    lang = 'ru.json'
+# Finnish
+elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=fi_FI.UTF-8':
+    lang = 'fi.json'
+# French
+elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=fr_FR.UTF-8':
+    lang = 'fr.json'
+# Norwegian (Bokmål)
+elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=nb_NO.UTF-8':
+    lang = 'nb_NO.json'
+# Ukrainian
+elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=uk_UA.UTF-8':
+    lang = 'uk.json'
+# Spanish
+elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=es_ES.UTF-8':
+    lang = 'es.json'
+# English
+else:
+    lang = 'en.json'
+
+if os.path.exists("{}/.var/app/com.github.vikdevelop.timer/cache/{}".format(Path.home(), lang)):
+    locale = open("{}/.var/app/com.github.vikdevelop.timer/cache/{}".format(Path.home(), lang))
+else:
+    locale = open(f"/app/translations/{lang}")
 
 # Load JSON local file
 jT = json.load(locale)
