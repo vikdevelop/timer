a219fc7208f86170f42fc6caab7b140f5489fad7
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2023-02-11 12:05:34 +0100

    Update CHANGELOG.py

--- src/CHANGELOG.py
+++ src/CHANGELOG.py
@@ -8,24 +8,24 @@ release_29 = '<p>v2.9\
     </li>minor UI changes</li>\
 </ul>'
 
-release_28 = "<p>2.8</p>\
+release_28 = "<p>v2.8</p>\
 <ul>\
     <li>The keyboard shortcuts Ctrl+P to pause the timer and Ctrl+T to continue in timing are now used instead of the original F6 and F7</li>\
     <li>Custom text for notification is now possible also use for alarm clock dialog</li>\
     <li>Minor UI changes</li>\
 </ul>"
         
-release_27_11 = "<p>2.7_0111</p>\
+release_27_11 = "<p>v2.7_0111</p>\
 <ul>\
 <li>Added option for pause timer (it is possible use button on headerbar next to Stop button or use Keyboard shortcuts F6 for pause, or F7 for continue)</li>\
 </ul>"
 
-release_27I = "<p>2.7I</p>\
+release_27I = "<p>v2.7I</p>\
 <ul>\
 <li>Improved Keyboard shortcuts window and function of keyboard shortcut that activates system and dark theme</li>\
 </ul>"
 
-release_27 = '<p>2.7</p>\
+release_27 = '<p>v2.7</p>\
 <ul>\
 <li>added option to action after finished timer "Play alarm clock". On timeout will be played sound until stopped and show message dialog</li>\
 <li>added Spanish translations, thanks to @haggen88</li>\
