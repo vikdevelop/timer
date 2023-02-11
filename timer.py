e895c7e28f899131b4d4360d1a37411389dbb075
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2023-02-11 14:27:53 +0100

    Update timer.py

--- timer.py
+++ timer.py
@@ -44,10 +44,23 @@ if os.path.exists("{}/.var/app/com.github.vikdevelop.timer/cache/{}".format(Path
 else:
     locale = open(f"/app/translations/{lang}")
 """
-
-with open(f"/app/translations/{lang}") as l:
-    # Load JSON local file
-    jT = json.load(l)
+try:
+    with open(f"/app/translations/{lang}") as l:
+        # Load JSON local file
+        jT = json.load(l)
+except:
+    jT["advanced"] = "More settings",
+    jT["go_to_more_settings"] = "Go to More settings",
+    jT["go_to_notification_settings"] = "Go to Notification settings",
+    jT["alternative_key"] = "The alternative keyboard shortcut is: {}",
+    jT["vertical_text"] = "Vertical text in countdown page",
+    jT["vertical_text_desc"] = "Displays vertical text that counts down the time on the countdown page",
+    jT["save_expander_row_state"] = "Expand Preferences dropdown row",
+    jT["save_expander_row_state_desc"] = "When you start the Timer application, the Preferences drop-down row will automatically expand.",
+    jT["show_appicon"] = "Show application icon in notification",
+    jT["show_appname"] = "Show application name in notification",
+    jT["notification_text"] = "<b>Notification</b>",
+    jT["alarm_clock_dialog_text"] = "<b>Alarm clock dialog</b>",
 
 # import main file - source code
 import src.main
