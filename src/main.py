56577e092ab8c8b5d478d6c1bfa971867d731834
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2023-02-11 12:03:28 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1385,7 +1385,7 @@ class MyApp(Adw.Application):
         dialog = Adw.AboutWindow(transient_for=app.get_active_window())
         dialog.set_application_name(jT["timer_title"])
         dialog.set_version("2.9")
-        dialog.set_release_notes(release_28 + release_27_11 + release_27I + release_27)
+        dialog.set_release_notes(release_29 + release_28 + release_27_11 + release_27I + release_27)
         dialog.set_developer_name("vikdevelop")
         self.add_translations_link(dialog)
         dialog.set_license_type(Gtk.License(Gtk.License.GPL_3_0))
