10ede295e163f038a6a11879b4ab0d46d4ee01e4
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2023-03-18 17:54:03 +0100

    Add Hungarian

--- timer.py
+++ timer.py
@@ -43,36 +43,16 @@ elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=pt_BR.UTF-8':
 # Arabic
 elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=ar_SA.UTF-8':
     lang = 'ar.json'
+# Hungarian
+elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=hu_HU.UTF-8':
+    lang = 'hu.json'
 # English
 else:
     lang = 'en.json'
-
-"""
-if os.path.exists("{}/.var/app/com.github.vikdevelop.timer/cache/{}".format(Path.home(), lang)):
-    try:
-        locale = open("{}/.var/app/com.github.vikdevelop.timer/cache/{}".format(Path.home(), lang))
-    except KeyError:
-        locale = open(f"/app/translations/{lang}")
-else:
-    locale = open(f"/app/translations/{lang}")
-"""
-try:
-    with open(f"/app/translations/{lang}") as l:
-        # Load JSON local file
-        jT = json.load(l)
-except:
-    jT["advanced"] = "More settings",
-    jT["go_to_more_settings"] = "Go to More settings",
-    jT["go_to_notification_settings"] = "Go to Notification settings",
-    jT["alternative_key"] = "The alternative keyboard shortcut is: {}",
-    jT["vertical_text"] = "Vertical text in countdown page",
-    jT["vertical_text_desc"] = "Displays vertical text that counts down the time on the countdown page",
-    jT["save_expander_row_state"] = "Expand Preferences dropdown row",
-    jT["save_expander_row_state_desc"] = "When you start the Timer application, the Preferences drop-down row will automatically expand.",
-    jT["show_appicon"] = "Show application icon in notification",
-    jT["show_appname"] = "Show application name in notification",
-    jT["notification_text"] = "<b>Notification</b>",
-    jT["alarm_clock_dialog_text"] = "<b>Alarm clock dialog</b>",
-
+    
+locale = open(f"/app/translations/{lang}")
+# Load JSON local file
+jT = json.load(locale)
+        
 # import main file - source code
 import src.main
