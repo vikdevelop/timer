373c03d84ec05688eb7fd770d470e7732db50e56
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2023-11-12 18:39:01 +0100

    improve getting language

--- timer.py
+++ timer.py
@@ -1,59 +1,22 @@
 import subprocess
 import json
 import os
+import locale
 from urllib.request import urlopen
 from pathlib import Path
 BASE_URL = 'https://raw.githubusercontent.com/vikdevelop/timer/main/translations/json'
-# Czech
-if subprocess.getoutput("locale | grep 'LANG'") == 'LANG=cs_CZ.UTF-8':
-    lang = 'cs.json'
-# Italian
-elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=it_IT.UTF-8':
-    lang = 'it.json'
-# Deutsch
-elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=de_DE.UTF-8':
-    lang = 'de.json'
-# Russian
-elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=ru_RU.UTF-8':
-    lang = 'ru.json'
-# Finnish
-elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=fi_FI.UTF-8':
-    lang = 'fi.json'
-# French
-elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=fr_FR.UTF-8':
-    lang = 'fr.json'
-# Norwegian (Bokmål)
-elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=nb_NO.UTF-8':
-    lang = 'nb_NO.json'
-# Ukrainian
-elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=uk_UA.UTF-8':
-    lang = 'uk.json'
-# Spanish
-elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=es_ES.UTF-8':
-    lang = 'es.json'
-# Turkish
-elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=tr_TR.UTF-8':
-    lang = 'tr.json'
-# Dutch
-elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=nl_NL.UTF-8':
-    lang = 'nl.json'
-# Portugalese (Brazil)
-elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=pt_BR.UTF-8':
-    lang = 'pt_BR.json'
-# Arabic
-elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=ar_SA.UTF-8':
-    lang = 'ar.json'
-# Hungarian
-elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=hu_HU.UTF-8':
-    lang = 'hu.json'
-# Persian
-elif subprocess.getoutput("locale | grep 'LANG'") == 'LANG=fa_IR.UTF-8':
-    lang = 'fa.json'
-# English
+# Load system language
+p_lang = locale.getlocale()[0]
+if p_lang == 'pt_BR':
+    r_lang = 'pt_BR'
+elif p_lang == 'nb_NO':
+    r_lang = 'nb_NO'
+elif 'zh' in p_lang:
+    r_lang = 'zh_Hans'
 else:
-    lang = 'en.json'
+    r_lang = p_lang[:-3]
     
-locale = open(f"/app/translations/{lang}")
+locale = open(f"/app/translations/{r_lang}.json")
 # Load JSON local file
 jT = json.load(locale)
         
